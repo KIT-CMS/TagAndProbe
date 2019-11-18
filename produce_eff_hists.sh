dd71ba8dd484fb86a47e752450dcbe7e102e73ec
Maximilian Burkart <maxiburkart@gmail.com>
2019-11-18 09:29:25 +0100

    [Tau] Add script for the efficiency production.

---
+++ produce_eff_hists.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ERA=$1
+SUFFIX=$2
+INPUT=$3
+MVA=$4
+
+MVA_ARG=""
+if [ $MVA == 1 ]
+then
+    MVA_ARG="--mva"
+fi
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh
+
+echo "[INFO] Producing efficiencies for ${ERA}.."
+if [[ $ERA =~ "2016" ]]; then
+    python scripts/TauTriggerEfficiency.py -w vloose loose medium tight vtight vvtight -i $INPUT -e $ERA -o output_${ERA}_tau_leg${SUFFIX}.root -f DATA MC -t mutau etau ditau --per-dm --fit -n 20 $MVA_ARG
+else
+    python scripts/TauTriggerEfficiency.py -w vloose loose medium tight vtight vvtight -i $INPUT -e $ERA -o output_${ERA}_tau_leg${SUFFIX}.root -f DATA MC -t mutau etau ditau --per-dm --fit -n 20 $MVA_ARG #--use-et
+fi
+
+
+echo "[INFO] Creating 2D efficiency maps for era ${ERA}.."
+./scripts/translate_2d_hists.py -i output_${ERA}_tau_leg${SUFFIX}.root -o etaphimapKIT_${ERA}${SUFFIX}.root -e $ERA
