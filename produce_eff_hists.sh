30f20b80eb3337088434a3aa89675f61a904f8b6
Maximilian Burkart <maxiburkart@gmail.com>
2020-09-25 18:07:51 +0200

    Update hostname command and split commands per era

--- produce_eff_hists.sh
+++ produce_eff_hists.sh
@@ -10,22 +10,27 @@ then
     MVA_ARG="--mva"
 fi
 
-if [[ "$hostname" =~ "bms2" ]]
+if [[ `hostname` =~ "bms2" ]]
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh
-elif [[ "$hostname" =~ "bms1" ]] || [[ "$hostname" =~ "bms3" ]]
+elif [[ `hostname` =~ "bms1" ]] || [[ `hostname` =~ "bms3" ]]
 then 
     source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc7-opt/setup.sh
 else
-    echo "[FATAL] Host currently not known. Aborting..."
+    echo "[FATAL] Host `hostname` currently not known. Aborting..."
     exit 1
 fi
 
 echo "[INFO] Producing efficiencies for ${ERA}.."
 if [[ $ERA =~ "2016" ]]; then
-    python scripts/TauTriggerEfficiency.py -w vloose loose medium tight vtight vvtight -i $INPUT -e $ERA -o output_${ERA}_tau_leg${SUFFIX}.root -f DATA MC -t mutau etau ditau --per-dm --fit -n 20 $MVA_ARG
+    python scripts/TauTriggerEfficiency.py -w vvvloose vvloose vloose loose medium tight vtight vvtight -i $INPUT -e $ERA -o output_${ERA}_tau_leg${SUFFIX}.root -f DATA MC EMB -t mutau etau ditau --per-dm --fit -n 20 $MVA_ARG
+elif [[ "$ERA" =~ "2017" ]]; then
+    python scripts/TauTriggerEfficiency.py -w vvvloose vvloose vloose loose medium tight vtight vvtight -i $INPUT -e $ERA -o output_${ERA}_tau_leg${SUFFIX}.root -f DATA MC EMB -t mutau etau ditau --per-dm --fit -n 20 $MVA_ARG #--use-et
+elif [[ "$ERA" =~ "2018" ]]; then
+    python scripts/TauTriggerEfficiency.py -w vvvloose vvloose vloose loose medium tight vtight vvtight -i $INPUT -e $ERA -o output_${ERA}_tau_leg${SUFFIX}.root -f DATA MC EMB -t mutau etau ditau ditauvbf --per-dm --fit -n 20 $MVA_ARG #--use-et
 else
-    python scripts/TauTriggerEfficiency.py -w vloose loose medium tight vtight vvtight -i $INPUT -e $ERA -o output_${ERA}_tau_leg${SUFFIX}.root -f DATA MC -t mutau etau ditau --per-dm --fit -n 20 $MVA_ARG #--use-et
+    echo "[FATAL] Era $ERA not known. Aborting..."
+    exit 1
 fi
 
 
