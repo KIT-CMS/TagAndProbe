83c36e1a0e293ea4c1a77a2f13d38c0814139908
Sebastian Brommer <sebastian.brommer@kit.edu>
2023-06-15 11:57:47 +0200

    update docs

--- README.md
+++ README.md
@@ -1,80 +1,182 @@
 # TagAndProbe
-Muon and electron tag and probe measurements and plotting
+Muon and electron tag and probe measurements and plotting. The Framework is used to measure several correction factors including
 
-**Setting up the code**
+* Selection Efficiency of the DoubleMuon trigger used for Tau-Embedding samples
+* Selection Efficiency of the Muon ID used for Tau-Embedding samples
 
-Either use the checkout script provided under `scripts/checkout.sh` or do the following:
+* Muon ID Scale Factors
+* Muon Isolation Scale Factors
+* Muon Trigger Scale Factors
+
+* Electron ID Scale Factors
+* Electron Isolation Scale Factors
+* Electron Trigger Scale Factors
+
+The following description of the workflow is using CROWN ntuples and was written in 06/2023. Several steps have to be performed, in order to obtain the finished correction factors:
+
+1. Production of Input NTuples using CROWN and Kingmaker
+2. Merging of intput Ntuples into one file for data, embedding and MC
+3. Creation of histograms for the tag and probe variables, based on the given Configuration
+4. Fitting of the histograms and creation of the final correction factors
+5. Plotting of the results
+6. Export of the Correction Factors in a valid correctionlib json file
+
+
+## 0. Installation
+To setup Kingmaker an CROWN, run
+
 ```bash
-export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
-source $VO_CMS_SW_DIR/cmsset_default.sh
-cmsrel CMSSW_12_3_2
-cd CMSSW_12_3_2/src
-cmsenv
-
-git clone --recursive git@github.com:KIT-CMS/TagAndProbe.git UserCode/TagAndProbe
-
-scramv1 b
+git clone --recursive git@github.com:KIT-CMS/KingMaker.git
+cd KingMaker
+source setup.sh KingMaker
 ```
 
-**First step**
+This will set up an instance of Kingmaker, including a checkout of CROWN.
+In order to add the CROWN configuration needed for the tag and probe measurements, run
 
-Create the input files using KIT-Higgs (https://github.com/KIT-CMS/KITHiggsToTauTau):
-
-`HiggsToTauTauAnalysis.py -a tagandprobe -i path_to_skimmed_filelist.txt`
-
-
-## Running tag and probe for electrons and muon
-
-
-1. Set the path to the inputfiles in 
-
-`set_inputfiles.yaml`
-
-2. Create the pass and fail histograms using
-
-`python scripts/TagAndProbe.py --channel (muon|electron|crossmuon|crosselectron) --era (2016|2017|2018)`
-
-3. Run the fits and plot the results
-
-`python scripts/runTagAndProbeFits.py --channel (muon|electron|crossmuon|crosselectron) --fit --plot --era (2016|2017|2018)`
-
-
-
-## Running tag and probe for hadronic taus (Outdated - use GP fit from Tau POG now)
-
-
-1. Get a clean shell environment (no CMSSW can be sourced yet) and do
- * for CentOS7 machines:
-`source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc7-opt/setup.sh`
- * for SLC6 machines: 
-`source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh`
-
-2. Run the fits and plot the results
-
-`python scripts/TauTriggerEfficiency.py  -i /ceph/jbechtel/artus_outputs/TagAndProbe/TauTrigger/new/ --fit --plot --era (2016|2017|2018) --per-dm --use-et`
-
-
-**Obtaining final results for trigger efficiencies for hadronic taus**
-
-1. Extract the 2D histograms from the created output file and create binned distributions from them.
-
-`python scripts/translate_2d_hists.py -i output_(2016|2017|2018)_tau_leg.root -o etaphi_out.root -e (2016|2017|2018)`
-
-2. Get the tau pog codes to run the fits and read out the efficiencies.
 ```bash
-cd $CMSSW_BASE/src
-git clone git@github.com:cms-tau-pog/TauTriggerTools.git
-git clone -b run2_SFs https://github.com/cms-tau-pog/TauTriggerSFs $CMSSW_BASE/src/TauAnalysisTools/TauTriggerSFs
-scram b -j 8
+git clone git@github.com:KIT-CMS/TauAnalysis-CROWN.git CROWN/analysis_configurations/tau
+```
+Finally, the TagAndProbe Framework is setup using the `checkout.sh` script in the `scripts` folder. This will create a CMSSW environment and checkout the TagAndProbe Framework into the `src` folder. The script can be run using
+
+```bash
+wget https://raw.githubusercontent.com/KIT-CMS/TagAndProbe/master/scripts/checkout.sh
+source checkout.sh
 ```
 
-3. Perform the fits of the 1D efficiency histograms. They are performed using the script `TauTriggerTools/TauTriggerFitTool/python/produceFitResults.py`. In order to do so the created output file has to be put under `TauTriggerTools/TauTriggerFitTool/data` and the corresponding script has to be adapted to the new file.
 
-4. Copy the resulting file to `$CMSSW_BASE/src/TauAnalysisTools/TauTriggerSFs/data` and run
+## 1. Creating the input files
+
+The configuration used for the TagAndProbe NTuples is located in the collection of Tau Configurations for CROWN. They can be found in the https://github.com/KIT-CMS/TauAnalysis-CROWN repository. For the creation of the TagAndProbe ntuples, the [`tauembedding_tagandprobe.py`](https://github.com/KIT-CMS/TauAnalysis-CROWN/blob/main/tauembedding_tagandprobe.py) configuration is used. Additional information on how CROWN configurations work and how they are used can be found in the [CROWN documentation](https://crown.readthedocs.io/en/latest/py_configuration.html). Adapt the configuration according to your needs.
+
+
+After the configuration is setup, the user has to define the samples, he wants to process. The most convenient way to do so is to use the `sample_manager` from KingMaker. First make sure, that the KingMaker environment is setup correctly in your shell (The setup is done using `source setup.sh KingMaker`), then run the Manager using
+
 ```bash
-cd $CMSSW_BASE/src/TauAnalysisTools/TauTriggerSFs/
-python copyEfficiencies.py
+sample_manager
 ```
-Here again the script has to be adapted to the correct paths.
 
-5. Add the fit results and the previously created file containing the 2D distributions to a common root file.
+which will give you an interactive CLI to manage your samples. To create a new production file, select `Create a production file`
+```
+The database contains 581 samples, split over 4 era(s) and 22 sampletype(s)
+? What do you want to do? (Use arrow keys)
+   ○ Add a new sample
+   ○ Edit a sample (not implemented yet)
+   ○ Delete a sample
+   ○ Find samples (by nick)
+   ○ Find samples (by DAS name)
+   ○ Print details of a sample
+ » ○ Create a production file
+   ○ Update genweight
+   ○ Save and Exit
+   ○ Exit without Save
+  Answer: Create a production file
+```
+In the next step, select the eras you want to use using the arrow keys and space bar
+
+```
+ Select eras to be added  (Use arrow keys to move, <space> to select, <a> to toggle, <i> to invert)
+   ○ 2016postVFP
+   ○ 2016preVFP
+   ● 2018
+ » ● 2017
+```
+
+and then selection the sampleypes you want to process. For the TagAndProbe measurement, this should be `embedding`, `data` and `dyjets`. Finally, chose a filename, and the manager will create a .txt file containing all selected samples. Double check, that the file contains all samples you want to process.
+
+To produce the ntuples, run
+
+```bash
+law run ProduceSamples --analysis tau --config tauembedding_tagandprobe --sample-list your_samplesfile.txt --production-tag your_productiontag --workers 100 --scopes mm,ee
+```
+
+The different options are:
+
+* `--analysis` : The analysis to be run. This is used to select the correct configuration file. The analysis is always `tau` for the TagAndProbe ntuples.
+* `--config` : The configuration to be used. This is the name of the configuration file without the `.py` extension. For the TagAndProbe ntuples, this is `tauembedding_tagandprobe`.
+* `--sample-list` : The name of the file containing the samples to be processed. This is the file created by the sample manager.
+* `--production-tag` : The production tag to be used. This is used to create the output directory. The samples will be stored on the grid, using this tag as identifier.
+* `--worker` : The number of workers to be used. One worker is able to submit and track one sample at a time. To submit and process all samples in parallel, use a high number of workers.
+* `--scopes`: The scopes to be used. This is a comma separated list of scopes. For the TagAndProbe ntuples, this should be `mm` for Emebdding Selection Corrections and Muon Scale Factors and `ee` for Electron Scale Factors.
+
+`law` will now process all samples listed in the `sample-list` file. This will take some time, depending on the number of samples and workers used. After the processing is finished, the output files will be stored on the grid. The default in KingMaker uses the T3 GridKa storage, a different storage location can be configured in the KingMaker configuration file.
+
+
+## 2. Merging the input files
+
+After the NTuples are produced, the [merge_outputs](./merge_outputs.sh) script via
+```
+./merge_outputs.sh your_productiontag (muon|electron) your_era
+```
+The script will collect the samples from the grid and merge them into one file per sample. The merged files will be stored in `/ceph/${USER}/Run2UltraLegacy/scalefactors/${your_productiontag}/${your_era}UL`. After the merging, the [set_inputfiles.yaml](./set_inputfiles.yaml) file has to be adapted to use the merged files as input.
+
+## 3. Configuring the scale factors
+
+The input files
+
+The Configurations can be found in the [configurations folder](./settings/UL/). The configuration files are named according to the era and channel, they are used for. They contain a list of different scale factors to measure. The configuration for each scale factor may look like the example shown below:
+
+```yaml
+Iso_pt_eta_bins:
+  name: "Iso_pt_eta_bins"
+  var: "m_vis(100,50,150)"
+  tag: " iso_tag < 0.15 && fsr_photon_veto_tag < 0.5 && fsr_photon_veto_probe < 0.5 && dz_tag < 0.2 && dxy_tag < 0.045 && id_medium_tag && trg_IsoMu27_tag && pt_tag > 28 && id_medium_probe"
+  probe: "iso_probe < 0.15"
+  binvar_x: "pt_probe"
+  bins_x:
+    [
+      10., 15., 20., 22., 24., 26., 28., 30., 32., 34., 36., 38., 40., 45., 50., 60., 80., 100., 200., 1000.,
+    ]
+  binvar_y: "abs(eta_probe)"
+  bins_y: [0, 0.9, 1.2, 2.1, 2.4]
+  # Settings for fits
+  BKG: "Exponential"
+  SIG: "DoubleVCorr"
+  # Settings for plotting
+  TITLE: "Iso_{#mu,rel} < 0.15"
+  y_range: [0.6, 1.0]
+  ratio_y_range: [0.8, 1.2]
+  info: "Scale factor for the muon isolation iso_mu < 0.15 (binned in eta). Scale factor is provided for Embedding (emb) and Simulation (mc)"
+  header: "Scale factor for the muon isolation iso_mu < 0.15 (binned in eta)"
+```
+
+The different required settings are:
+
+* `name`: The name of the scale factor. This is used to name the output files.
+* `var`: The variable used for the histogram. This is the variable used for the fit. Usually, the mass of the dilepton system called `m_vis` is used.
+* `tag`: The selection string used to define the tag lepton. Internally, the terms `_tag` and `_probe` are replaced by `_1` and `_2` to ensure, that every lepton can be used as tag and probe.
+* `probe`: The selection string used to define the probe lepton. This is the quantity, for which the efficiency is measured.
+* `binvar_x`: The variable used for the x-axis of the histogram. This is usually the `pt` of the probe lepton.
+* `bins_x`: The binning used for the x-axis of the histogram. This is a list of bin edges.
+* `binvar_y`: The variable used for the y-axis of the histogram. This is usually the `eta` of the probe lepton.
+* `bins_y`: The binning used for the y-axis of the histogram. This is a list of bin edges.
+* `BKG`: The background model used for the fit. This is the name of the background model as defined in the [./scripts/fitTagAndProbe_script.py](./scripts/fitTagAndProbe_script.py) file.
+* `SIG`: The signal model used for the fit. This is the name of the signal model as defined in the [./scripts/fitTagAndProbe_script.py](./scripts/fitTagAndProbe_script.py) file.
+* `TITLE`: The title of the histogram. This is used for plotting.
+* `y_range`: The range of the y-axis of the efficiency plot.
+* `ratio_y_range`: The range of the y-axis of the ratio plot.
+* `info`: A short description of the scale factor. This is used for the correctionlib file.
+* `header`: A short description of the scale factor. This is used for the correctionlib file.
+
+## 4. Running the fits
+
+Finally, the scale factors are calculated using
+
+```bash
+python3 scripts/TagAndProbe.py --channel (embeddingselection|muon|electron) --era your_era
+python3 scripts/runTagAndProbeFits.py --channel  (embeddingselection|muon|electron) --era your_era --fit --plot
+```
+the resulting fits and efficiency plots are all stored in the `output` folder. They should be checked carefully, to and all previous steps should be repeated if necessary.
+
+
+## Creating the correctionlib files
+
+In the last step, the scale factors are stored in correctionlib files. This is done using the [translate_to_crosspog_json.py](./scripts/translate_to_crosspog_json.py) script. The script can be run using
+
+```bash
+python3 scripts/translate_to_crosspog_json.py -e your_era -c (embeddingselection|muon|electron) -o output
+```
+
+Congratulations, you have created the scale factors for the UL Run2 Legacy analysis!
+
+
--- merge_outputs.sh
+++ merge_outputs.sh
@@ -5,21 +5,21 @@ CHANNEL=$2
 ERA=$3
 
 case ${CHANNEL} in
-    "electron")
-        scope=ee
-        sample="Electron"
-        ;;
+"electron")
+    scope=ee
+    sample="Electron"
+    ;;
 
-    "muon")
-        scope=mm
-        sample="Muon"
-        ;;
+"muon")
+    scope=mm
+    sample="Muon"
+    ;;
 
-    *)
-        echo "[ERROR] Given channel ${CHANNEL} not implemented"
-        exit 42
+*)
+    echo "[ERROR] Given channel ${CHANNEL} not implemented"
+    exit 42
+    ;;
 esac
-
 
 basedir=/store/user/${USER}/CROWN/ntuples/${NTUPLETAG}/CROWNRun/${ERA}/
 gridpath=root://cmsxrootd-kit.gridka.de:1094/
@@ -27,54 +27,191 @@ output_dir=/ceph/${USER}/Run2UltraLegacy/scalefactors/${NTUPLETAG}/${ERA}UL
 [[ ! -d ${output_dir} ]] && mkdir -p ${output_dir}
 
 # Write out the merge commands for all sample types
-# MC
-[[ -f merge_DYJetsToLL_files_${ERA}_${scope}.log ]] && rm merge_DYJetsToLL_files_${ERA}_${scope}.log
-for ext in "" _ext1; do 
-    xrdfs ${gridpath} ls ${basedir}/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIISummer20UL${ERA: -2}NanoAODv9-106X${ext}/${scope}/
-done >> merge_DYJetsToLL_files_${ERA}_${scope}.log
-# Modify the written file by appending the redirector to get the full path
-sed -i "s#^#${gridpath}#" merge_DYJetsToLL_files_${ERA}_${scope}.log
-case ${ERA} in
+if [[ ${ERA} == "2018" ]]; then
+    # MC
+    [[ -f merge_DYJetsToLL_files_${ERA}_${scope}.log ]] && rm merge_DYJetsToLL_files_${ERA}_${scope}.log
+    for ext in "" _ext1; do
+        xrdfs ${gridpath} ls ${basedir}/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIISummer20UL${ERA: -2}NanoAODv9-106X${ext}/${scope}/
+    done >>merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # Data
+    echo "[INFO] Gathering data file list..."
+    [[ -f merge_Single${sample}_files_${ERA}_${scope}.log ]] && rm merge_Single${sample}_files_${ERA}_${scope}.log
+    for period in {A..D}; do
+        xrdfs ${gridpath} ls ${basedir}/Single${sample}_Run${ERA}${period}-UL${ERA}/${scope}/
+    done >>merge_Single${sample}_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_Single${sample}_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_Single${sample}_files_${ERA}_${scope}.log
 
-    "2017")
-        # Data
-        echo "[INFO] Gathering data file list..."
-        [[ -f merge_Single${sample}_files_${ERA}_${scope}.log ]] && rm merge_Single${sample}_files_${ERA}_${scope}.log
-        for period in {B..F}; do
-            xrdfs ${gridpath} ls ${basedir}/Single${sample}_Run${ERA}${period}-UL${ERA}/${scope}/
-        done >> merge_Single${sample}_files_${ERA}_${scope}.log
+    if [[ ${CHANNEL} == "muon" ]]; then
+        [[ -f merge_Double${sample}_files_${ERA}_${scope}.log ]] && rm merge_Double${sample}_files_${ERA}_${scope}.log
+        for period in {A..D}; do
+            xrdfs ${gridpath} ls ${basedir}/Double${sample}_Run${ERA}${period}-UL${ERA}/${scope}/
+        done >>merge_Double${sample}_files_${ERA}_${scope}.log
         # Modify the written file by appending the redirector to get the full path
-        sed -i "s#^#${gridpath}#" merge_Single${sample}_files_${ERA}_${scope}.log
+        sed -i "s#^#${gridpath}#" merge_Double${sample}_files_${ERA}_${scope}.log
+        # remove lines containing .json files
+        sed -i '/\.json/d' merge_Double${sample}_files_${ERA}_${scope}.log
+    fi
+    # Embedding
+    echo "[INFO] Gathering embedding file list..."
+    [[ -f merge_${sample}Embedding_files_${ERA}_${scope}.log ]] && rm merge_${sample}Embedding_files_${ERA}_${scope}.log
+    for period in {A..D}; do
+        xrdfs ${gridpath} ls ${basedir}/${sample}Embedding_Run${ERA}${period}-UL${ERA}/${scope}/
+    done >>merge_${sample}Embedding_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_${sample}Embedding_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_${sample}Embedding_files_${ERA}_${scope}.log
 
-        if [[ ${CHANNEL} == "muon" ]]; then
-            [[ -f merge_Double${sample}_files_${ERA}_${scope}.log ]] && rm merge_Double${sample}_files_${ERA}_${scope}.log
-            for period in {B..F}; do
-                xrdfs ${gridpath} ls ${basedir}/Double${sample}_Run${ERA}${period}-UL${ERA}/${scope}/
-            done >> merge_Double${sample}_files_${ERA}_${scope}.log
-            # Modify the written file by appending the redirector to get the full path
-            sed -i "s#^#${gridpath}#" merge_Double${sample}_files_${ERA}_${scope}.log
-        fi
 
-        # Embedding
-        echo "[INFO] Gathering embedding file list..."
-        [[ -f merge_${sample}Embedding_files_${ERA}_${scope}.log ]] && rm merge_${sample}Embedding_files_${ERA}_${scope}.log
+elif [[ ${ERA} == "2017" ]]; then
+    # MC
+    [[ -f merge_DYJetsToLL_files_${ERA}_${scope}.log ]] && rm merge_DYJetsToLL_files_${ERA}_${scope}.log
+    for ext in "" _ext1; do
+        xrdfs ${gridpath} ls ${basedir}/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIISummer20UL${ERA: -2}NanoAODv9-106X${ext}/${scope}/
+    done >>merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # Data
+    echo "[INFO] Gathering data file list..."
+    [[ -f merge_Single${sample}_files_${ERA}_${scope}.log ]] && rm merge_Single${sample}_files_${ERA}_${scope}.log
+    for period in {B..F}; do
+        xrdfs ${gridpath} ls ${basedir}/Single${sample}_Run${ERA}${period}-UL${ERA}/${scope}/
+    done >>merge_Single${sample}_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_Single${sample}_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_Single${sample}_files_${ERA}_${scope}.log
+
+    if [[ ${CHANNEL} == "muon" ]]; then
+        [[ -f merge_Double${sample}_files_${ERA}_${scope}.log ]] && rm merge_Double${sample}_files_${ERA}_${scope}.log
         for period in {B..F}; do
-            xrdfs ${gridpath} ls ${basedir}/${sample}Embedding_Run${ERA}${period}-UL${ERA}/${scope}/
-        done >> merge_${sample}Embedding_files_${ERA}_${scope}.log
+            xrdfs ${gridpath} ls ${basedir}/Double${sample}_Run${ERA}${period}-UL${ERA}/${scope}/
+        done >>merge_Double${sample}_files_${ERA}_${scope}.log
         # Modify the written file by appending the redirector to get the full path
-        sed -i "s#^#${gridpath}#" merge_${sample}Embedding_files_${ERA}_${scope}.log
-        ;;
+        sed -i "s#^#${gridpath}#" merge_Double${sample}_files_${ERA}_${scope}.log
+        # remove lines containing .json files
+        sed -i '/\.json/d' merge_Double${sample}_files_${ERA}_${scope}.log
+    fi
+    # Embedding
+    echo "[INFO] Gathering embedding file list..."
+    [[ -f merge_${sample}Embedding_files_${ERA}_${scope}.log ]] && rm merge_${sample}Embedding_files_${ERA}_${scope}.log
+    for period in {B..F}; do
+        xrdfs ${gridpath} ls ${basedir}/${sample}Embedding_Run${ERA}${period}-UL${ERA}/${scope}/
+    done >>merge_${sample}Embedding_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_${sample}Embedding_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_${sample}Embedding_files_${ERA}_${scope}.log
 
-    *)
-        echo "[ERROR] Not implemented yet..."
-        exit 42
-        ;;
-esac
 
-hadd ${output_dir}/DYJetsToLL_${ERA}UL_${scope}.root $(cat merge_DYJetsToLL_files_${ERA}_${scope}.log | tr '\n' ' ') &
-hadd ${output_dir}/Single${sample}_${ERA}UL.root $(cat merge_Single${sample}_files_${ERA}_${scope}.log | tr '\n' ' ') &
-if [[ ${CHANNEL} == "muon" ]]; then
-    hadd ${output_dir}/Double${sample}_${ERA}UL.root $(cat merge_Double${sample}_files_${ERA}_${scope}.log | tr '\n' ' ') &
+elif [[ ${ERA} == "2016postVFP" ]]; then
+    echo "[INFO] Gathering file list for ${ERA}..."
+    ERA_SAMPLENAME="2016"
+    # MC
+    [[ -f merge_DYJetsToLL_files_${ERA}_${scope}.log ]] && rm merge_DYJetsToLL_files_${ERA}_${scope}.log
+    for ext in "" _ext1; do
+        xrdfs ${gridpath} ls ${basedir}/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIISummer20UL${ERA_SAMPLENAME: -2}NanoAODv9-106X${ext}/${scope}/
+    done >>merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # Data
+    echo "[INFO] Gathering data file list..."
+    [[ -f merge_Single${sample}_files_${ERA}_${scope}.log ]] && rm merge_Single${sample}_files_${ERA}_${scope}.log
+    for period in {F..H}; do
+        xrdfs ${gridpath} ls ${basedir}/Single${sample}_Run${ERA_SAMPLENAME}${period}-UL${ERA_SAMPLENAME}/${scope}/
+    done >>merge_Single${sample}_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_Single${sample}_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_Single${sample}_files_${ERA}_${scope}.log
+
+    if [[ ${CHANNEL} == "muon" ]]; then
+        [[ -f merge_Double${sample}_files_${ERA}_${scope}.log ]] && rm merge_Double${sample}_files_${ERA}_${scope}.log
+        for period in {F..H}; do
+            xrdfs ${gridpath} ls ${basedir}/Double${sample}_Run${ERA_SAMPLENAME}${period}-UL${ERA_SAMPLENAME}/${scope}/
+        done >>merge_Double${sample}_files_${ERA}_${scope}.log
+        # Modify the written file by appending the redirector to get the full path
+        sed -i "s#^#${gridpath}#" merge_Double${sample}_files_${ERA}_${scope}.log
+        # remove lines containing .json files
+        sed -i '/\.json/d' merge_Double${sample}_files_${ERA}_${scope}.log
+    fi
+    # Embedding
+    echo "[INFO] Gathering embedding file list..."
+    [[ -f merge_${sample}Embedding_files_${ERA}_${scope}.log ]] && rm merge_${sample}Embedding_files_${ERA}_${scope}.log
+    for period in {F..H}; do
+        xrdfs ${gridpath} ls ${basedir}/${sample}Embedding_Run${ERA_SAMPLENAME}${period}-UL${ERA_SAMPLENAME}/${scope}/
+    done >>merge_${sample}Embedding_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_${sample}Embedding_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_${sample}Embedding_files_${ERA}_${scope}.log
+
+
+elif [[ ${ERA} == "2016preVFP" ]]; then
+    echo "[INFO] Gathering file list for ${ERA}..."
+    ERA_SAMPLENAME="2016"
+    declare -a data_period=("B-ver1" "B-ver2" "C-HIPM" "D-HIPM" "E-HIPM" "F-HIPM")
+    declare -a emb_period=("B_ver1" "B_ver2" "C" "D" "E" "F")
+    # MC
+    [[ -f merge_DYJetsToLL_files_${ERA}_${scope}.log ]] && rm merge_DYJetsToLL_files_${ERA}_${scope}.log
+
+    xrdfs ${gridpath} ls ${basedir}/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIISummer20UL16NanoAODAPVv9-106X/${scope}/ >> merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_DYJetsToLL_files_${ERA}_${scope}.log
+    # Data
+    echo "[INFO] Gathering data file list..."
+    [[ -f merge_Single${sample}_files_${ERA}_${scope}.log ]] && rm merge_Single${sample}_files_${ERA}_${scope}.log
+    for period in ${data_period[@]}; do
+        xrdfs ${gridpath} ls ${basedir}/Single${sample}_Run${ERA_SAMPLENAME}${period}/${scope}/
+    done >>merge_Single${sample}_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_Single${sample}_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_Single${sample}_files_${ERA}_${scope}.log
+
+    if [[ ${CHANNEL} == "muon" ]]; then
+        [[ -f merge_Double${sample}_files_${ERA}_${scope}.log ]] && rm merge_Double${sample}_files_${ERA}_${scope}.log
+        for period in ${data_period[@]}; do
+            xrdfs ${gridpath} ls ${basedir}/Double${sample}_Run${ERA_SAMPLENAME}${period}/${scope}/
+        done >>merge_Double${sample}_files_${ERA}_${scope}.log
+        # Modify the written file by appending the redirector to get the full path
+        sed -i "s#^#${gridpath}#" merge_Double${sample}_files_${ERA}_${scope}.log
+        # remove lines containing .json files
+        sed -i '/\.json/d' merge_Double${sample}_files_${ERA}_${scope}.log
+    fi
+    # Embedding
+    echo "[INFO] Gathering embedding file list..."
+    [[ -f merge_${sample}Embedding_files_${ERA}_${scope}.log ]] && rm merge_${sample}Embedding_files_${ERA}_${scope}.log
+    for period in ${emb_period[@]}; do
+        xrdfs ${gridpath} ls ${basedir}/${sample}Embedding_Run${ERA_SAMPLENAME}-HIPM_${period}-UL${ERA_SAMPLENAME}/${scope}/
+    done >>merge_${sample}Embedding_files_${ERA}_${scope}.log
+    # Modify the written file by appending the redirector to get the full path
+    sed -i "s#^#${gridpath}#" merge_${sample}Embedding_files_${ERA}_${scope}.log
+    # remove lines containing .json files
+    sed -i '/\.json/d' merge_${sample}Embedding_files_${ERA}_${scope}.log
+else
+
+    echo "[ERROR] Era ${ERA} Not implemented yet..."
+    exit 42
 fi
-hadd ${output_dir}/${sample}Embedding_${ERA}UL.root $(cat merge_${sample}Embedding_files_${ERA}_${scope}.log | tr '\n' ' ') &
+
+hadd -f ${output_dir}/DYJetsToLL_${ERA}UL_${scope}.root $(cat merge_DYJetsToLL_files_${ERA}_${scope}.log | tr '\n' ' ') &
+hadd -f ${output_dir}/Single${sample}_${ERA}UL.root $(cat merge_Single${sample}_files_${ERA}_${scope}.log | tr '\n' ' ') &
+if [[ ${CHANNEL} == "muon" ]]; then
+    hadd -f ${output_dir}/Double${sample}_${ERA}UL.root $(cat merge_Double${sample}_files_${ERA}_${scope}.log | tr '\n' ' ') &
+fi
+hadd -f ${output_dir}/${sample}Embedding_${ERA}UL.root $(cat merge_${sample}Embedding_files_${ERA}_${scope}.log | tr '\n' ' ') &
 wait
