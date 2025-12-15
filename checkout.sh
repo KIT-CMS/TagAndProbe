45a688db48d68e2a9b159e21fa236f03c3724aa6
agaganidze <alexander.gaganidze@student.kit.edu>
2025-12-15 10:06:30 +0100

    removing jupyter nb from gitignore adding simple setup script

---
+++ checkout.sh
@@ -0,0 +1,15 @@
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+cmsrel CMSSW_12_3_2
+cd CMSSW_12_3_2/src
+cmsenv
+
+mkdir UserCode 
+cd UserCode
+
+git clone --recursive git@github.com:KIT-CMS/TagAndProbe.git
+
+cd -
+
+scramv1 b
+
